66ed535dd18da9137dba342885c7457ef078bbd8
Golda Velez <golda@3box.io>
2023-04-02 15:17:46 -0700

    some database utilities for nodes edges and claims

--- claims-to-nodes/pipe.py
+++ claims-to-nodes/pipe.py
@@ -1,6 +1,7 @@
 import psycopg2
 from ..lib.db_config import DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT
 from ..lib.cleaners import normalize_uri
+from ..lib.db import get_node_by_uri, get_edge_by_endpoints
    
 
 # Connect to the PostgreSQL database
@@ -18,25 +19,26 @@ cur = conn.cursor()
 # Read data from the Claim model
 cur.execute("SELECT id, subject, claim, object, statement, effectiveDate, sourceURI, howKnown, dateObserved, digestMultibase, author, curator, aspect, score, stars, amt, unit, howMeasured, intendedAudience, respondAt, confidence, issuerId, issuerIdType, claimAddress, proof FROM Claim WHERE edges IS NULL")
 
-# Process the data and output the derived models
-nodes = {}
-edges = []
 
-def add_or_create_node(node_uri, row):
+def get_or_create_node(node_uri, row, new_node=None):
 
     node_uri = normalize_uri(node_uri, row['issuerId'])
-    node = nodes.get(subject_uri)
+    node = get_node_by_uri(node_uri)
     if node is None:
-        (name, thumbnail_uri) = infer_details(node_uri, save_thumbnail=True)
-        entType = 'ORGANIZATION' # TODO infer or default to UNKNOWN for later edit
-        node = {
-            "nodeUri": node_uri,
-            "name": name,
-            "entType": entType,
-            "thumbnail": thumbnail_uri,
-            "descrip": ''
-        }
-        nodes[node_uri] = node
+        if new_node is None:
+            (name, thumbnail_uri) = infer_details(node_uri, save_thumbnail=True)
+            entType = 'ORGANIZATION' # TODO infer or default to UNKNOWN for later edit
+            node = {
+                "nodeUri": node_uri,
+                "name": name,
+                "entType": entType,
+                "thumbnail": thumbnail_uri,
+                "descrip": ''
+            }
+        else:
+            node = new_node
+        insert_node(node)
+    # TODO possibly update the node if exists
     return node 
 
 for row in cur.fetchall():
@@ -44,35 +46,36 @@ for row in cur.fetchall():
     subject_node = add_or_create_node(row['subject'], row)
     object_node = None
 
-    if row['object'] is not None:
+    object_uri = row['object']
+    if object_uri is not None:
         object_node = add_or_create_node(row['object'], row) 
-   
-    if row['sourceURI'] is not None:
+  
+    source_uri = row['sourceURI'] 
+    if source_uri is not None:
         source_node = add_or_create_node(row['sourceURI'], row)
  
     # Create the claim node
     claim_uri = row['claimAddress']
-    claim_node = {
+    claim = row['claim']
+    claim_node = add_or_create_node(claim_uri, row, {
         "nodeUri": claim_uri,
-        "name": row['claim'],
+        "name": claim, 
         "entType": "Claim",
         "descrip": row['statement']  # TODO use the other fields too
-    }
-    nodes[claim_uri] = claim_node
+    })
+   
     
     # Create the edge from the subject node to the claim node
     subject_to_claim_edge = {
-        "id": len(edges) + 1,
         "startNode": subject_node,
         "startNodeId": subject_node["id"],
         "endNode": claim_node,
         "endNodeId": claim_node["id"],
-        "label": "claim"
+        "label": claim
     }
     edges.append(subject_to_claim_edge)
     
     # Create the edge from the claim node to the source node (if available)
-    source_uri = row[6]
     if source_uri is not None:
         source_node = nodes.get(source_uri)
         if source_node is None:
---
+++ lib/db.py
@@ -0,0 +1,100 @@
+import psycopg2
+
+from ..lib.db_config import DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT
+
+# Connect to the PostgreSQL database
+def get_conn():
+    global conn
+    try:
+        # check if conn is open
+        conn.status
+    except (AttributeError, psycopg2.OperationalError):
+        # conn is closed or doesn't exist yet
+        conn = psycopg2.connect(
+            database=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            host=DB_HOST,
+            port=DB_PORT
+        )
+    return conn
+
+def unprocessed_claims_generator():
+    with get_conn().cursor() as cur:
+        # Read data from the Claim model
+        cur.execute("SELECT id, subject, claim, object, statement, effectiveDate, sourceURI, howKnown, dateObserved, digestMultibase, author, curator, aspect, score, stars, amt, unit, howMeasured, intendedAudience, respondAt, confidence, issuerId, issuerIdType, claimAddress, proof FROM Claim WHERE edges IS NULL")
+
+        while True:
+            rows = cur.fetchmany()
+            if not rows:
+                break
+            for row in rows:
+                yield row
+
+
+def execute_sql_query(query, params):
+    with get_conn().cursor() as cur:
+        cur.execute(query, params)
+        result = cur.fetchone()
+        conn.commit()
+        return result
+
+def insert_data(table, data):
+    conn = get_conn()
+    query = f"INSERT INTO {table} ({', '.join(data.keys())}) VALUES ({', '.join(['%s']*len(data))}) RETURNING id;"
+    return execute_sql_query(conn, query, tuple(data.values()))[0]
+
+def insert_node(node):
+    """Insert a Node into the database."""
+    node_id = get_node_id_by_uri(conn, node['nodeUri'])
+    if node_id:
+        return node_id
+    return insert_data(table='Node', data=node)
+
+def insert_edge(edge):
+    """Insert an Edge into the database."""
+    return insert_data(table='Edge', data=edge)
+
+def get_node_by_uri(node_uri):
+    """Retrieve a Node from the database by its nodeUri value."""
+    select_node_sql = """
+        SELECT id, nodeUri, name, entType, descrip, image, thumbnail
+        FROM Node
+        WHERE nodeUri = %s;
+    """
+    row = execute_sql_query(select_node_sql, (node_uri,))
+    if row is None:
+        return None
+    node_dict = {
+            'id': row['id'],
+            'nodeUri': row['nodeUri'],
+            'name': row['name'],
+            'entType': row['entType'],
+            'descrip': row['descrip'],
+            'image': row['image'],
+            'thumbnail': row['thumbnail']
+        }
+    return node_dict
+
+def get_edge_by_endpoints(start_node_id, end_node_id):
+    """Retrieve an Edge from the database by the IDs of its start and end Nodes."""
+    select_edge_sql = """
+        SELECT id, startNodeId, endNodeId, label, thumbnail, claimId
+        FROM Edge
+        WHERE startNodeId = %s AND endNodeId = %s;
+    """
+    row = execute_sql_query(select_edge_sql, (start_node_id, end_node_id))
+    if row is None:
+        return None
+
+    edge_dict = {
+            'id': row['id'],
+            'startNodeId': row['startNodeId'],
+            'endNodeId': row['endNodeId'],
+            'label': row['label'],
+            'thumbnail': row['thumbnail'],
+            'claimId': row['claimId']
+        }
+    return edge_dict
+
+
