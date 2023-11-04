8c1a748eed6b95d68d7ea92eed5dfeed71a21192
supersonicwisd1 <supersonicwisdom@gmail.com>
2023-11-04 03:59:02 +0100

    adding multiple hypercert data to DB done

--- import_claims/hypercerts/import.py
+++ import_claims/hypercerts/import.py
@@ -1,56 +1,130 @@
 import json
 import pandas as pd
 import re
+import psycopg2
+import validators
+import urllib
+import sys
+from dotenv import dotenv_values
 
-# Defining the CSV file and the fields to extract
+config = dotenv_values("../.env")
+
+def print_error_and_exit(str):
+    print(f"Error: {str}")
+    sys.exit(2)
+
+def db_post_one(query, values):
+    conn = None
+    try:
+        conn = psycopg2.connect(
+            config.get("DATABASE_URL")
+        )
+        cursor = conn.cursor()
+
+        cursor.execute(query, values)
+        conn.commit()
+    except (Exception, psycopg2.Error) as error:
+        print("Failed insertion: {}".format(error))
+        print_error_and_exit(error)
+    finally:
+        if conn:
+            cursor.close()
+            conn.close()
+
+def db_post_many(query, values):
+    conn = None
+    try:
+        conn = psycopg2.connect(
+            config.get("DATABASE_URL")
+        )
+        cursor = conn.cursor()
+
+        cursor.executemany(query, values)
+        conn.commit()
+    except (Exception, psycopg2.Error) as error:
+        print("Failed insertion: {}".format(error))
+        print_error_and_exit(error)
+    finally:
+        if conn:
+            cursor.close()
+            conn.close()
+
+def extract_config_data(row, subject_from_csv_header, statement_from_csv_header, object_from_csv_header):
+    scraped_meta_str = row[object_from_csv_header].replace("'", "\"")
+    ipfs_uri_match = re.search(r'"ipfs_uri":\s*"([^"]+)"', scraped_meta_str)
+
+    if ipfs_uri_match:
+        ipfs_uri = ipfs_uri_match.group(1)
+    else:
+        ipfs_uri = ""
+
+    config_data = {
+        "filename": csv_filename,
+        "subject": row[subject_from_csv_header],
+        "subject_type": "ORGANISATION",
+        "object": ipfs_uri,
+        "statement": row[statement_from_csv_header],
+        "claim": "is_vouched_for",
+        "aspect": "trust",
+        "howknown": "WEB_DOCUMENT",
+        "score": 1.0,
+        "source_from_csv_header": object_from_csv_header,
+        "confidence": 0.9
+    }
+
+    return config_data
+
 csv_filename = "hypercert-claim-data.csv"
 subject_from_csv_header = "name"
 statement_from_csv_header = "description"
 object_from_csv_header = "scraped_meta"
 
-# Loading the CSV file into a pandas DataFrame
 try:
     df = pd.read_csv(csv_filename)
 except FileNotFoundError:
-    print(f"Error: File not found. Is \"{csv_filename}\" a valid path?")
-    exit(1)
+    print(f"Error: File not found. Is \'{csv_filename}\' a valid path?")
+    sys.exit(1)
 
-# Extract data from the first row of the CSV
-first_row = df.iloc[0]
+query = """INSERT INTO "Claim" (subject, object, statement, claim, aspect, "howKnown", "reviewRating", source, confidence, "userId", "issuerId", "issuerIdType") VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
 
-# getting the data for source 
-scraped_meta_str = first_row[object_from_csv_header].replace("'", "\"")
-ipfs_uri_match = re.search(r'"ipfs_uri":\s*"([^"]+)"', scraped_meta_str)
+for index, row in df.iterrows():
+    config_data = extract_config_data(row, subject_from_csv_header, statement_from_csv_header, object_from_csv_header)
+    values = (
+        config_data["subject"],
+        config_data["object"],
+        config_data["statement"],
+        config_data["claim"],
+        config_data["aspect"],
+        config_data["howknown"],
+        config_data.get("score", 1.0),
+        config_data.get("source_from_csv_header", ""),
+        config_data.get("confidence", 0.9),
+        config.get("spider_id", 1),
+        f"http://trustclaims.whatscookin.us/users/{config.get('spider_id')}",
+        "URL"
+    )
 
-if ipfs_uri_match:
-    ipfs_uri = ipfs_uri_match.group(1)
-else:
-    ipfs_uri = ""
+    
+    db_post_many(query, values)
 
-# Create a dictionary with the extracted data
-config_data = {
-    "filename": csv_filename,
-    "subject": "",  # You can set this to a default value or extract it from the CSV
-    "subject_from_csv_header": subject_from_csv_header,
-    "subject_type": "ORGANISATION",
-    "object": ipfs_uri,  # Set "object" to the "ipfs_uri"
-    "object_from_csv_header": object_from_csv_header,
-    "statement": first_row[statement_from_csv_header],  # Set statement to the "description" value
-    "statement_from_csv_header": statement_from_csv_header,
-    "claim": "is_vouched_for",
-    "aspect": "trust",
-    "howknown": "WEB_DOCUMENT",
-    "score": 1.0,
-    "source_from_csv_header": object_from_csv_header,
-    "confidence": 0.9
-}
+# config_data = extract_config_data(csv_filename, subject_from_csv_header, statement_from_csv_header, object_from_csv_header)
 
-# Update the fields in the dictionary with the extracted data
-config_data["subject"] = first_row[subject_from_csv_header]
+# query = """INSERT INTO "Claim" (subject, object, statement, claim, aspect, "howKnown", "reviewRating", source, confidence, "userId", "issuerId", "issuerIdType") VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+# values = (
+#     config_data["subject"],
+#     config_data["object"],
+#     config_data["statement"],
+#     config_data["claim"],
+#     config_data["aspect"],
+#     config_data["howknown"],
+#     config_data.get("score", 1.0),
+#     config_data.get("source_from_csv_header", ""),
+#     config_data.get("confidence", 0.9),
+#     1,
+#     f"http://trustclaims.whatscookin.us/users/{config.get('spider_id')}",
+#     "URL"
+# )
 
-# Save the updated dictionary to the config.json file
-with open("config.json", "w") as config_file:
-    json.dump(config_data, config_file, indent=4)
+# db_post_one(query, values)
 
-# verify if the process went well
-print("config.json has been updated with data from the CSV.")
+print("Data has been inserted into the PostgreSQL database.")
