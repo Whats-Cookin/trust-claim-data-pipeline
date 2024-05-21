a9151fcdbd0e46c0c430bd5bf5f06273223e1a1f
Golda Velez <gvelez17@gmail.com>
2024-05-20 22:01:54 -0700

    use a resolvable url on the live site

--- claims_to_nodes/pipe.py
+++ claims_to_nodes/pipe.py
@@ -92,7 +92,7 @@ def process_claim(raw_claim):
                 source_node = get_or_create_node(raw_claim['sourceURI'], raw_claim)
 
             # Create the claim node
-            claim_uri = raw_claim['claimAddress'] or 'https://linkedtrust.us/claims/{}'.format(raw_claim['id'])
+            claim_uri = raw_claim['claimAddress'] or 'https://live.linkedtrust.us/claims/{}'.format(raw_claim['id'])
             claim_node = get_or_create_node(claim_uri, raw_claim, {
                 "nodeUri": claim_uri,
                 "name": raw_claim['claim'], 
