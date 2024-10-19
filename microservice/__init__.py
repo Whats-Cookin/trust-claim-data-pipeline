a6fa49cf39a8400be7ef7a5576e2306aa7462236
Omar Eloui <contact@omareloui.com>
2024-10-19 18:26:57 +0300

    chore(microservice): add logs

--- microservice/__init__.py
+++ microservice/__init__.py
@@ -1,6 +1,27 @@
+from logging.config import dictConfig
+
 from flask import Flask, jsonify
 
 from claims_to_nodes.pipe import process_targeted
+
+dictConfig(
+    {
+        "version": 1,
+        "formatters": {
+            "default": {
+                "format": "[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
+            }
+        },
+        "handlers": {
+            "wsgi": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://flask.logging.wsgi_errors_stream",
+                "formatter": "default",
+            }
+        },
+        "root": {"level": "INFO", "handlers": ["wsgi"]},
+    }
+)
 
 app = Flask(__name__)
 
@@ -8,7 +29,9 @@ app = Flask(__name__)
 @app.route("/process_claim/<claim_id>", methods=["POST"])
 def process_claim(claim_id):
     try:
+        app.logger.info(f'Signing claim with id "{claim_id}"')
         process_targeted(claim_id)
         return jsonify({"message": "Claim processed successfully"}), 200
     except Exception as e:
+        app.logger.error(f'Failed to sign claim with id: "{claim_id}"')
         return jsonify({"error": str(e)}), 500
