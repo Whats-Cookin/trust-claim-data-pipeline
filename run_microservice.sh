84ea95c71bb3d72fd15f75e3821c49f82fa839e7
LinkedTrust <connect@linkedtrust.us>
2025-11-30 21:55:19 +0000

    ensure the env is activated

--- run_microservice.sh
+++ run_microservice.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /data/trust-claim-data-pipeline
 export PYTHONPATH=/data/trust-claim-data-pipeline
-venv/bin/python microservice/__init__.py
+source venv/bin/activate
+python microservice/__init__.py
